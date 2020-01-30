efe0a6625a3c68feb1b42206914e51419680bf26
paulk444 <paulk444@hotmail.com>
2020-01-29 16:36:11 -0800

    Mainly added hp_tune_aa1.sh

--- human_ai_robustness/plot_expt_results.py
+++ human_ai_robustness/plot_expt_results.py
@@ -3,10 +3,12 @@ from human_aware_rl.ppo.ppo_tom import plot_ppo_tom
 
 SEEDS = [2732, 9845]
 
-DIR = '/home/pmzpk/Documents/hr_coordination_from_server_ONEDRIVE/final_tune_results/ppo_runs'
+DIR = '/home/pmzpk/Documents/hr_coordination_from_server_ONEDRIVE/hp_tune_except_croom'
 
-for i in range(5):
+for i in [0, 1, 2, 4, 5]:
 
-    run_name = 'croom_' + str(i)
+    run_name = 'aa_' + str(i)
 
+    if i > 1:
+        SEEDS = [2732]
     plot_ppo_tom(DIR, run_name, SEEDS)

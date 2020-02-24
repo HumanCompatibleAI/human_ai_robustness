73b9ba0c52b389bb981955f5ce67eda3dc73ca1e
paulk444 <paulk444@hotmail.com>
2020-02-24 16:55:03 +0000

    Mainly added some tom pop expts

--- human_ai_robustness/plot_expt_results.py
+++ human_ai_robustness/plot_expt_results.py
@@ -1,15 +1,14 @@
 
 from human_aware_rl.ppo.ppo_pop import plot_ppo_tom
 
-SEEDS = [3264, 4859, 9225]
-# SEEDS = [2732, 9845]
+SEEDS = [3264, 4859, 9225, 2732, 9845]
 
-DIR = '/home/pmzpk/Documents/hr_coordination_from_server_ONEDRIVE/pop_expt_aa'
-# DIR = /home/pmzpk/Dropbox/Pycharm_paulk444/human_ai_robustness/human_ai_robustness/data/actual_experiments
+# DIR = '/home/pmzpk/Documents/hr_coordination_from_server_ONEDRIVE/pop_expt_cring'
+DIR = '/home/pmzpk/Dropbox/Pycharm_paulk444/human_ai_robustness/human_ai_robustness/data/actual_experiments'
 
-for i in range(3):
+for i in range(1):
 
-    run_name = 'aa_' + str(i)
-    # run_name = 'aa_2'
+    # run_name = 'cring_' + str(i)
+    run_name = 'tom_pop_expt/cring_20_toms_p'
 
     plot_ppo_tom(DIR, run_name, SEEDS)

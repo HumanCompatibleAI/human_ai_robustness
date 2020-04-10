de1ab32c28ae68de90836830d3e1c55cb3b232d3
paulk444 <paulk444@hotmail.com>
2020-04-10 14:20:34 +0100

    Updated expt_all_toms_cc0.sh

--- human_ai_robustness/plot_expt_results.py
+++ human_ai_robustness/plot_expt_results.py
@@ -2,12 +2,12 @@
 from human_aware_rl.ppo.ppo_pop import plot_ppo_tom
 
 # SEEDS = [3264, 4859, 9225, 2732, 9845]
-SEEDS = [2732, 9845]
+SEEDS = [2732]
 
-DIR = '/home/pmzpk/Documents/hr_coordination_from_server_ONEDRIVE/val_expt_aa_cc1'
+DIR = '/home/pmzpk/Documents/hr_coordination_from_server_ONEDRIVE/all_toms_cc0'
 # DIR = '/home/pmzpk/Dropbox/Pycharm_paulk444/human_ai_robustness/human_ai_robustness/data/'
 
-runs = ['1_mantom', '20_mantoms', '1_mettom', '20_mettoms', '1_bc', '20_bcs']
+runs = ['tom_0_quick']
 map = 'cc'
 
 for i in range(len(runs)):
@@ -15,4 +15,4 @@ for i in range(len(runs)):
     run_name = '{}_{}'.format(map, runs[i])
     # run_name = 'tom_pop_expt/cring_20_toms_p'
 
-    plot_ppo_tom(DIR, run_name, SEEDS, plot_val=True)
+    plot_ppo_tom(DIR, run_name, SEEDS, plot_val=False)

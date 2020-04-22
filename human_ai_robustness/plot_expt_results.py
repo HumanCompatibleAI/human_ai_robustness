8c3535587fa57a440150054e951d1788d14d1c96
paulk444 <paulk444@hotmail.com>
2020-04-22 10:13:46 +0100

    Added hp_lstm_croom2.sh

--- human_ai_robustness/plot_expt_results.py
+++ human_ai_robustness/plot_expt_results.py
@@ -4,12 +4,12 @@ from human_aware_rl.ppo.ppo_pop import plot_ppo_tom
 # SEEDS = [3264, 4859, 9225, 2732, 9845]
 SEEDS = [2732]
 
-DIR = '/home/pmzpk/Documents/hr_coordination_from_server_ONEDRIVE/hp_lstm_croom0'
+DIR = '/home/pmzpk/Documents/hr_coordination_from_server_ONEDRIVE/hp_lstm_croom1'
 # DIR = '/home/pmzpk/Dropbox/Pycharm_paulk444/human_ai_robustness/human_ai_robustness/data/'
 
 # runs = ['tom_{}'.format(i) for i in range(12)]
 # map = 'cc'
-runs = ['{}'.format(i) for i in range(12)]
+runs = ['2b', '2c', '3b', '3c', '1b', '7b', '7c', '12', '13', '5c']
 map = 'croom'
 
 for i in range(len(runs)):

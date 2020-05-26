ce746293ef59e51463c119d209dc19e1ac9634de
Micah Carroll <mdc@berkeley.edu>
2020-05-25 23:49:51 -0700

    Added HH starts to PPO training

---
+++ human_ai_robustness/common.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def get_hh_states_start_state_fn(layout_name, data_type):
+    from human_aware_rl.human.process_dataframes import get_human_human_trajectories
+    if data_type == "train" or data_type is True:
+        hh_trajs_for_layout = get_human_human_trajectories([layout_name], "train")[layout_name]
+    elif data_type == "test":
+        hh_trajs_for_layout = get_human_human_trajectories([layout_name], "test")[layout_name]
+    else:
+        raise ValueError()
+
+    hh_starts = np.concatenate(hh_trajs_for_layout["ep_observations"])
+    start_state_fn = lambda: np.random.choice(hh_starts)
+    return start_state_fn

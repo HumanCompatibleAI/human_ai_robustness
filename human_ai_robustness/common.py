93cb7a55507798645698afec86c7d692b0124e71
paulk444 <paulk444@hotmail.com>
2020-05-26 16:25:43 +0100

    Added functionality for hh_data_starts Modified runner.py so that mixed populations now use the parallel action of the BC model

--- human_ai_robustness/common.py
+++ human_ai_robustness/common.py
@@ -10,5 +10,10 @@ def get_hh_states_start_state_fn(layout_name, data_type):
         raise ValueError()
 
     hh_starts = np.concatenate(hh_trajs_for_layout["ep_observations"])
+
+    # Need to set the order list for the TOMs to work with these hh_starts:
+    for i in range(len(hh_starts)):
+        hh_starts[i].order_list = ["any"] * 100
+
     start_state_fn = lambda: np.random.choice(hh_starts)
     return start_state_fn

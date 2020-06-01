958e5b28cfb6ce5814d3ff74ac5243d2021f2ae3
paulk444 <paulk444@hotmail.com>
2020-06-01 10:21:12 +0100

    Added alltoms ppo agents

--- human_ai_robustness/plot_expt_results.py
+++ human_ai_robustness/plot_expt_results.py
@@ -3,16 +3,17 @@ from human_aware_rl.ppo.ppo_pop import plot_ppo_tom
 import matplotlib.pyplot as plt
 
 SEEDS = [2732, 3264, 4859, 9225, 9845]
+# SEEDS = [2732, 4859, 9845]
 # SEEDS = [9845]
 
-map = 'cring'
+map = 'cc'
 
-DIR = '/home/pmzpk/Documents/hr_coordination_from_server_ONEDRIVE/neu_expt_{}0'.format(map)
+DIR = '/home/pmzpk/Documents/hr_coordination_from_server_ONEDRIVE/neu_expt_{}_alltoms'.format(map)
 # DIR = '/home/pmzpk/Dropbox/Pycharm_paulk444/human_ai_robustness/human_ai_robustness/data/'
 
 # runs = ['{}'.format(i) for i in range(11)]
 # map = 'cc'
-runs = ['1tom_ns', '20tom_ns', '1bc_ns', '20bc_ns', '20mixed_ns', '20mixed_s']
+runs = ['1tom5', '1tom7', '1tom9', '1tom11']
 # runs = ['2b', '2c', '3b', '3c', '1b', '7b', '7c', '12', '13', '5c', '5b']
 
 for i in range(len(runs)):
--- human_ai_robustness/qt_cc_cring0.sh
+++ human_ai_robustness/qt_cc_cring0.sh
@@ -10,3 +10,4 @@ CUDA_VISIBLE_DEVICES=0 python qualitative_robustness_expt.py -l counter_circuit 
 CUDA_VISIBLE_DEVICES=1 python qualitative_robustness_expt.py -l counter_circuit -a_n final_neurips_agents/cc/cc_20bc_ns -n 30
 CUDA_VISIBLE_DEVICES=2 python qualitative_robustness_expt.py -l counter_circuit -a_n final_neurips_agents/cc/cc_20mixed_ns -n 30
 CUDA_VISIBLE_DEVICES=3 python qualitative_robustness_expt.py -l counter_circuit -a_n final_neurips_agents/cc/cc_20mixed_s -n 30
+

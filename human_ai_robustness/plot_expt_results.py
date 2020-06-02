1018ca7037a5a70850ffa6a84c31573894584af5
paulk444 <paulk444@hotmail.com>
2020-06-02 11:52:18 +0100

    Added more final ppo agents

--- human_ai_robustness/plot_expt_results.py
+++ human_ai_robustness/plot_expt_results.py
@@ -6,14 +6,14 @@ SEEDS = [2732, 3264, 4859, 9225, 9845]
 # SEEDS = [2732, 4859, 9845]
 # SEEDS = [9845]
 
-map = 'cc'
+map = 'cpot'
 
-DIR = '/home/pmzpk/Documents/hr_coordination_from_server_ONEDRIVE/neu_expt_{}_alltoms'.format(map)
+DIR = '/home/pmzpk/Documents/hr_coordination_from_server_ONEDRIVE/neu_expt_cobj_cpot0'.format(map)
 # DIR = '/home/pmzpk/Dropbox/Pycharm_paulk444/human_ai_robustness/human_ai_robustness/data/'
 
 # runs = ['{}'.format(i) for i in range(11)]
 # map = 'cc'
-runs = ['1tom5', '1tom7', '1tom9', '1tom11']
+runs = ['1tom', '20tom', '1bc', '20bc']
 # runs = ['2b', '2c', '3b', '3c', '1b', '7b', '7c', '12', '13', '5c', '5b']
 
 for i in range(len(runs)):

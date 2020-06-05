f4095ca205b4b532a21a66aa190d48d3974f8adc
paulk444 <paulk444@hotmail.com>
2020-06-05 12:08:14 +0100

    Bug fix

--- human_ai_robustness/qt_actual_1BC.sh
+++ human_ai_robustness/qt_actual_1BC.sh
@@ -1,6 +1,6 @@
-CUDA_VISIBLE_DEVICES=0 python qualitative_robustness_expt.py -l counter_circuit -a_n bc -n 50 -nv 5
-CUDA_VISIBLE_DEVICES=1 python qualitative_robustness_expt.py -l coordination_ring -a_n bc -n 50 -nv 5
-CUDA_VISIBLE_DEVICES=2 python qualitative_robustness_expt.py -l bottleneck -a_n bc -n 50 -nv 5
-CUDA_VISIBLE_DEVICES=3 python qualitative_robustness_expt.py -l large_room -a_n bc -n 50 -nv 5
-CUDA_VISIBLE_DEVICES=0 python qualitative_robustness_expt.py -l centre_objects -a_n bc -n 50 -nv 5
-CUDA_VISIBLE_DEVICES=1 python qualitative_robustness_expt.py -l centre_pots -a_n bc -n 50 -nv 5
+CUDA_VISIBLE_DEVICES=0 python qualitative_robustness_expt.py -l counter_circuit -a_t bc -n 50 -nv 5
+CUDA_VISIBLE_DEVICES=1 python qualitative_robustness_expt.py -l coordination_ring -a_t bc -n 50 -nv 5
+CUDA_VISIBLE_DEVICES=2 python qualitative_robustness_expt.py -l bottleneck -a_t bc -n 50 -nv 5
+CUDA_VISIBLE_DEVICES=3 python qualitative_robustness_expt.py -l large_room -a_t bc -n 50 -nv 5
+CUDA_VISIBLE_DEVICES=0 python qualitative_robustness_expt.py -l centre_objects -a_t bc -n 50 -nv 5
+CUDA_VISIBLE_DEVICES=1 python qualitative_robustness_expt.py -l centre_pots -a_t bc -n 50 -nv 5
